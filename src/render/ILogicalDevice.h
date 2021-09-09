a03d93745ec8b8502a1b5ca0839505d31249a1e4
waty2246 <tuana1648@gmail.com>
2021-09-09 17:17:42 +0700

    Add a note the intended use of ILogicalDevice and IPhysicalDevice

--- src/render/ILogicalDevice.h
+++ src/render/ILogicalDevice.h
@@ -1,1 +1,16 @@
 #pragma once
+
+namespace sp
+{
+    namespace render
+    {
+        /// <summary>
+        /// Manage access to the core inner functions of the Graphics API, such as creating graphics data structures like textures, buffers, queues, pipelines.
+        /// </summary>
+        class ILogicalDevice
+        {
+        public:
+            virtual ~ILogicalDevice(){}
+        };
+    }
+}
--- src/render/IPhysicalDevice.h
+++ src/render/IPhysicalDevice.h
@@ -1,1 +1,16 @@
 #pragma once
+
+namespace sp
+{
+    namespace render
+    {
+        /// <summary>
+        /// Used to query physical device specific details such as video memory size and features support (MSAA ... ).
+        /// </summary>
+        class IPhysicalDevice
+        {
+        public:
+            virtual ~IPhysicalDevice(){}
+        };
+    }
+}
